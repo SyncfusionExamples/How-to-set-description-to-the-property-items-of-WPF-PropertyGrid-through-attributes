c51191317b31f4df71d3980785a4e74497f4e57a
Durga-Rajan <31245621+Durga-Rajan@users.noreply.github.com>
2019-10-14 11:22:15 +0530

    Add files via upload

---
+++ PropertyGrid-DescriptionSettings/BooleanToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PropertyGrid_DescriptionSettings
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility && (Visibility)value == Visibility.Visible)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+    }
+
+}
---
+++ PropertyGrid-DescriptionSettings/Person.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using Syncfusion.Windows.PropertyGrid;
+using Syncfusion.Windows.Shared;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Windows.Media.Imaging;
+using System.Reflection;
+
+namespace PropertyGrid_DescriptionSettings
+{
+    public class Person
+    {
+        public Person()
+        {
+            FirstName = "Carl";
+            LastName = "Johnson";
+            Age = 30;
+            Mobile = 91983467382;
+            Email = "carljohnson@gta.com";
+            ID = "0005A";
+            DOB = new DateTime(1987, 10, 16);
+            Designation = "Team Lead";
+        }
+
+        [Display(Description = "Email address of the employee")]
+        public string Email
+        {
+            get;
+
+            set;
+        }
+
+        [Display(Description = "First Name of the employee")]
+        public string FirstName
+        {
+            get;
+
+            set;
+        }
+
+        [Display(Description = "Position of the employee in the organisation")]
+        public string Designation
+        {
+            get;
+
+            set;
+        }
+
+
+        [Display(Description = "Last Name of the employee")]
+        public string LastName
+        {
+            get;
+
+            set;
+        }
+
+        [DescriptionAttribute("ID of the employee")]
+        public string ID
+        {
+            get;
+
+            set;
+        }
+
+
+        [DescriptionAttribute("Birth date of the employee")]
+        public DateTime DOB
+        {
+            get;
+
+            set;
+        }
+
+
+        [DescriptionAttribute("Contact Number of the employee")]
+        public long Mobile
+        {
+            get;
+
+            set;
+        }
+
+        [DescriptionAttribute("Age of the employee")]
+        public int Age
+        {
+            get;
+
+            set;
+        }       
+    }
+}
